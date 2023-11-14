68d3b3c9444f7592ba743e84b586f205a0707177
Andrey Smorodin <smorod1n@yandex.ru>
2023-11-14 19:59:28 +0300

    add program

---
+++ Program/Program.cs
@@ -0,0 +1,40 @@
+﻿string[] FilterArray(string[] inputArray)
+{
+    int count = 0;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] outputArray = new string[count];
+    int index = 0;
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            outputArray[index] = inputArray[i];
+            index++;
+        }
+    }
+
+    return outputArray;
+}
+
+Console.Write("Введите элементы массива, разделенные запятой: ");
+string input = Console.ReadLine() ?? "";
+
+string[] inputArray = input.Split(',');
+string[] outputArray = FilterArray(inputArray);
+
+Console.WriteLine("Результат:");
+Console.Write("[");
+foreach (string item in outputArray)
+{
+    Console.Write($"\"{item}\",");
+}
+Console.Write("]");
+Console.WriteLine();
